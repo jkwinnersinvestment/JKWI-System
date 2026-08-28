bda97725769033e14e5cec5cc3ee707fc583e507
jkwinnersinvestment <info@jkwinnersinvestment.co.za>
2026-08-28 14:55:26 +0200

    Prepare JKWI for Railway

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,30 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Onka$123",
-    database: "jkwi_system"
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306
 });
 
 connection.connect((error) => {
+
     if (error) {
-        console.error("Database connection failed:", error);
+
+        console.error(
+            "Database connection failed:",
+            error
+        );
+
     } else {
-        console.log("JKWI Database Connected Successfully");
+
+        console.log(
+            "JKWI Database Connected Successfully"
+        );
+
     }
+
 });
 
 module.exports = connection;
